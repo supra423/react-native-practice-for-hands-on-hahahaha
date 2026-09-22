ef5c58085e67eeea128fceeb135528ac3017fdfc
supra <alexusenzoroa@gmail.com>
2026-09-22 12:31:40 +0800

    used file-based routing

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { LoginScreen } from '../src/LoginScreen';
+import { StyleSheet, View } from 'react-native';
+
+export default function Index() {
+	return <View style={styles.container}><LoginScreen /></View>
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
